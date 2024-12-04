71ac97d6475b75525ef2b92e0ff76c00b553ccf7
Frnak <fddewaard@gmail.com>
2024-12-04 09:54:00 +0100

    part two, q&d because technically i'm in a meeting

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -10,12 +10,39 @@ import java.util.regex.Pattern;
 public class Day4 {
     private final List<String> lines;
     Pattern pattern = Pattern.compile("(?=(XMAS|SAMX))");
+    Pattern patternPart2 = Pattern.compile("MAS|SAM");
     public Day4() {
         lines = new InputHelper(4).getLines();
     }
     public static void main(String... args) {
         Day4 solver = new Day4();
         System.out.println("XMAS count: " + solver.partOne());
+        System.out.println("XMAS count: " + solver.partTwo());
+    }
+
+    private int partTwo() {
+        String[][] array = new String[lines.size()][];
+        for (int i = 0; i < lines.size(); i++) {
+            array[i] = new String[lines.get(i).length()];
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                array[i][j] = String.valueOf(lines.get(i).charAt(j));
+            }
+        }
+        int counter = 0;
+
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array[i].length-1; j++) {
+                if (array[i][j].equalsIgnoreCase("A") && i > 0 && j > 0) {
+                    String string1 = array[i-1][j-1] + array[i][j] + array[i+1][j+1];
+                    String string2 = array[i+1][j-1] + array[i][j] + array[i-1][j+1];
+                    if (patternPart2.matcher(string1).find() && patternPart2.matcher(string2).find()) {
+                       counter++;
+                    }
+                }
+            }
+        }
+
+        return counter;
     }
 
     private int partOne() {
