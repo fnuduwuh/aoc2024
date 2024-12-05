3966c5128c0a6994081bd0611f85b2dfb18ada68
Frank de Waard <frank.dewaard@duo.nl>
2024-12-05 15:45:14 +0100

    day5 part two

--- src/day5/Day5.java
+++ src/day5/Day5.java
@@ -20,12 +20,15 @@ public class Day5 {
 
     public static void main(String... args) {
         Day5 solver = new Day5();
-        solver.partOne();
+        solver.solve();
     }
 
-    private void partOne() {
+    private void solve() {
         List<List<String>> safeSequences = new ArrayList<>();
+        List<List<String>> unsafeSequences = new ArrayList<>();
         int count = 0;
+        int countPt2 = 0;
+
         this.lines.forEach(line -> {
             if (line.contains("|")) {
                 String[] parts = line.split("\\|");
@@ -39,6 +42,8 @@ public class Day5 {
                 List<String> nums = new ArrayList<>(Arrays.asList(line.split(",")));
                 if (!line.isEmpty() && this.checkSafety(nums)) {
                     safeSequences.add(Arrays.asList(line.split(",")));
+                } else if (!line.isEmpty()) {
+                    unsafeSequences.add(fixOrderAndGetMiddleNumber(line.split(",")));
                 }
             }
         });
@@ -46,17 +51,34 @@ public class Day5 {
         for (List<String> seq : safeSequences) {
             count += Integer.parseInt(seq.get(seq.size() / 2));
         }
+        for (List<String> seq : unsafeSequences) {
+            countPt2 += Integer.parseInt(seq.get(seq.size() / 2));
+        }
 
-        System.out.println(count);
+        System.out.println("Part one: " + count);
+        System.out.println("Part two: " + countPt2);
     }
 
-    private boolean checkSafety(List<String> nums) {
-        for (int i = nums.size() - 1; i > 0; i--) {
-            String numToCheck = nums.remove(i);
+    private List<String> fixOrderAndGetMiddleNumber(String[] line) {
+        Arrays.sort(line, (o1, o2) -> {
+            if (orderingRules.get(o1) != null && orderingRules.get(o1).contains(o2)) {
+                return -1;
+            } else if (orderingRules.get(o2) != null && orderingRules.get(o2).contains(o1)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        return new ArrayList<>(Arrays.asList(line));
+    }
+
+    private boolean checkSafety(List<String> numbers) {
+        for (int i = numbers.size() - 1; i > 0; i--) {
+            String numToCheck = numbers.remove(i);
             List<String> rules = orderingRules.get(numToCheck);
-            if (nums.isEmpty()) {
+            if (numbers.isEmpty()) {
                 return true;
-            } else if (rules != null && !Collections.disjoint(nums, rules)) {
+            } else if (rules != null && !Collections.disjoint(numbers, rules)) {
                 return false;
             }
         }
